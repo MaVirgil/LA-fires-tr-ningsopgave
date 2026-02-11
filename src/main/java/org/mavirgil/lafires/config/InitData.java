0e5788494bda08d266654fad4143b4983a397440
MaVirgil <magnusvirgil@hotmail.com>
2026-02-11 17:26:22 +0100

    test: add initial population of Fires

--- src/main/java/org/mavirgil/lafires/config/InitData.java
+++ src/main/java/org/mavirgil/lafires/config/InitData.java
@@ -1,5 +1,6 @@
 package org.mavirgil.lafires.config;
 
+import org.mavirgil.lafires.model.Fire;
 import org.mavirgil.lafires.model.Location;
 import org.mavirgil.lafires.model.Siren;
 import org.mavirgil.lafires.repository.FireRepository;
@@ -33,7 +34,16 @@ public class InitData implements CommandLineRunner {
         siren3.setLocation(new Location(34.18809678582704, -118.4361232858282));
 
         sirenRepository.saveAll(List.of(siren1, siren2, siren3));
-
         System.out.println("Saved sirens to database...");
+
+        Fire fire1 = new Fire();
+        fire1.setLocation(new Location(33.96618939424191, -118.27445192147037));
+
+        Fire fire2 = new Fire();
+        fire2.setLocation(new Location(33.85405889779855, -118.26521355779278));
+        fire2.setActive(true);
+
+        fireRepository.saveAll(List.of(fire1, fire2));
+        System.out.println("Saved fires to database...");
     }
 }
