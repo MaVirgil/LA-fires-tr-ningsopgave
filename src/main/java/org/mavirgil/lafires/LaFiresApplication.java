a3b2f44d0c783829bb40f23107481399446a018e
MaVirgil <magnusvirgil@hotmail.com>
2026-02-11 15:32:54 +0100

    initial commit

---
+++ src/main/java/org/mavirgil/lafires/LaFiresApplication.java
@@ -0,0 +1,13 @@
+package org.mavirgil.lafires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaFiresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaFiresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mavirgil/lafires/LaFiresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mavirgil.lafires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaFiresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
