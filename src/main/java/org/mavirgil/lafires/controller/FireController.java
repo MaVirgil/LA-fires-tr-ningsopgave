295c6b516870e29c3ffacc2b8be8485cfe21b523
MaVirgil <magnusvirgil@hotmail.com>
2026-02-11 17:04:18 +0100

    feat: add basic CRUD endpoints

--- src/main/java/org/mavirgil/lafires/controller/FireController.java
+++ src/main/java/org/mavirgil/lafires/controller/FireController.java
@@ -1,9 +1,13 @@
 package org.mavirgil.lafires.controller;
 
+import org.mavirgil.lafires.model.Fire;
+import org.mavirgil.lafires.model.Siren;
 import org.mavirgil.lafires.repository.FireRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/fire")
@@ -19,15 +23,58 @@ public class FireController {
                 GET ENDPOINTS
     -----------------------------------*/
 
+    @GetMapping
+    public ResponseEntity<List<Fire>> getFires() {
+
+        try {
+            return ResponseEntity.ok(repository.findAll());
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
     /*----------------------------------
                 POST ENDPOINTS
     -----------------------------------*/
+    @PostMapping
+    public ResponseEntity<Fire> addFire(@RequestBody Fire fire) {
+
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(fire));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 
     /*----------------------------------
                 PUT ENDPOINTS
     -----------------------------------*/
+    @PutMapping("/{id}")
+    public ResponseEntity<Fire> editFire(@PathVariable Integer id, @RequestBody Fire fire) {
+
+        try {
+            fire.setId(id);
+            return ResponseEntity.ok(repository.save(fire));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 
     /*----------------------------------
                 DELETE ENDPOINTS
     -----------------------------------*/
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteFire(@PathVariable Integer id) {
+
+        if (!repository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        try {
+            repository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
